25d1a62cfd88d7f881c7a883504b3e9e5f55fdad
annoamlw <annoamlw@sina.com>
2024-07-28 12:38:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,61 +6,54 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  USE_PASSAGE: 'push-deer',
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '福建',
-  CITY: '泉州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU31275TRj1PAi78jS30ZIxkJXyh6MYhzbBNVTQc',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '11-30',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    "APP_ID": "wxfffe83633c47f36a",
+    "APP_SECRET": "6dffbb63f057680f245c0c4cf565c9ce",
+    "IS_SHOW_COLOR": true,
+    "CALLBACK_TEMPLATE_ID": "",
+    "CALLBACK_USERS": [
         {
-          type: '*生日', name: '宝宝', year: '2002', date: '11-30',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+            "name": "自己",
+            "id": ""
+        }
+    ],
+    "USERS": [
         {
-          type: '生日',name: '宝宝', year: '2002', date: '11-30',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-01',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-07-01' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'op0Su6FL4trZo89kL8m8b_3donhc',
+            "name": "宝宝",
+            "id": "op0Su6FL4trZo89kL8m8b_3donhc",
+            "useTemplateId": "0001",
+            "province": "福建",
+            "city": "泉州",
+            "horoscopeDate": "11-30",
+            "horoscopeDateType": "本周",
+            "openUrl": "https://cdxiaodong.life/",
+            "festivals": [
+                {
+                    "type": "节日",
+                    "name": "恋爱纪念日",
+                    "date": "07-01",
+                    "year": "2024"
+                }
+            ],
+            "customizedDateList": [
+                {
+                    "keyword": "恋爱纪念日",
+                    "date": "2024-07-01"
+                }
+            ]
+        }
+    ],
+    "SWITCH": {
+        "weather": true,
+        "holidaytts": true,
+        "CIBA": true,
+        "oneTalk": false,
+        "earthyLoveWords": false,
+        "momentCopyrighting": false,
+        "poisonChickenSoup": false,
+        "poetry": false,
+        "horoscope": false,
+        "birthdayMessage": true
     }
-  ],
-
 }
 
 module.exports = USER_CONFIG
